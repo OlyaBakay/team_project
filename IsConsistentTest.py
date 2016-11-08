400fb14f7b4dac06262aad69212bbb493253cd56
Maryashka-Kudryashka <Maryashka3@gmail.com>
2016-11-09 00:20:25 +0200

    Update IsConsistentTest.py

--- IsConsistentTest.py
+++ IsConsistentTest.py
@@ -93,12 +93,12 @@ class Test(unittest.TestCase):
         self.assertEqual(expected, actual, "Error")
 
     def test_7_main(self):
-        expected = (([[1,0,0],[0,1,0],[0,0,1]],[1,0,0]),("System is consistent"))
+        expected = ([[1,0,0,1],[0,1,0,0],[0,0,1,0]],"System is consistent")
         actual = main([[1,0,0],[1,2,0],[1,2,3]],[1,1,1])
         self.assertEqual(expected, actual, "Error")
-        
+
     def test_8_main(self):
-        expected = (([[1, 0, 0,0,0,0], [0, 1, 0,0,0,0], [0, 0, 1,0,0,0],[0, 0,0,1,0,0],[0,0,0,0,1,0],[0,0,0,0,0,1]],[0.4,1.8,0.4,1,-1,-0.8]), 'System is consistent')
+        expected = ([[1, 0, 0,0,0,0,0.4], [0, 1, 0,0,0,0,1.8], [0, 0, 1,0,0,0,0.4],[0, 0,0,1,0,0,1],[0,0,0,0,1,0,-1],[0,0,0,0,0,1,-0.8]], 'System is consistent')
         actual = main([[1,2,0,0,3,0],[3,0,2,0,0,0],[1,1,0,1,0,4],[3,0,2,0,2,0],[0,1,2,0,1,2],[0,0,0,2,0,0]],[1,2,0,0,0,2])
         self.assertEqual(expected, actual, "Error")
 
@@ -220,16 +220,6 @@ class Test(unittest.TestCase):
     def test_4_add_vector(self):
         expected = "Wrong vector"
         actual = add_vector([[2,4,3,7,6,5,8,6,5,7],[4,6,5,8,7,6,5,5,4,6],[6,5,4,6,5,4,3,4,5,6]],[7,6,5,7])
-        self.assertEqual(expected, actual, "Error")
-
-    def test_1_for_arsen(self):
-        expected = ([[1,5,3,5,7,3,6,5,4,7],[5,3,8,6,1,4,6,7,5,2],[7,4,6,9,7,5,6,7,8,5]],[8,3,6])
-        actual = for_arsen([[1,5,3,5,7,3,6,5,4,7,8],[5,3,8,6,1,4,6,7,5,2,3],[7,4,6,9,7,5,6,7,8,5,6]])
-        self.assertEqual(expected, actual, "Error")
-
-    def test_2_for_arsen(self):
-        expected = ([[1,5,3,5,7],[5,3,8,6,1],[7,4,6,9,7]],[3,4,5])
-        actual = for_arsen([[1,5,3,5,7,3],[5,3,8,6,1,4],[7,4,6,9,7,5]])
         self.assertEqual(expected, actual, "Error")
 
 if __name__ == "__main__":
