2072232bb4197af726a093d4adf13c2bf47d0fa7
OlyaBakay <Olhabak@gmail.com>
2016-11-08 16:34:34 +0200

    Update IsConsistent.py

--- IsConsistent.py
+++ IsConsistent.py
@@ -9,18 +9,21 @@ def consist_of_zero(vector):
 
 def number_of_rows(matrix):
     return len(matrix)
+
+
 def number_of_colums(matrix):
     return len(matrix[0])
 
 
 def main(matrix_a, vector_b):
+    # написати функцію для перевірки чи правильна введена матриця
     if number_of_rows(matrix_a) == number_of_rows(vector_b):
         if consist_of_zero(vector_b):
             return "System is always consistent."
         else:
             final_matrix = matrix_a.add_vector(vector_b)
             final_matrix = to_reduced_row_echelon_form(final_matrix)
-            final_matrix = check(final_matrix)
+            final_matrix = check_system(final_matrix)
     else:
         return "System is inconsistent."
 
@@ -52,14 +55,15 @@ def to_reduced_row_echelon_form(matrix):
     return matrix
 
 
-def check(matrix):
+def check_system(matrix):
+    # перевіряє зведену матрицю. Аби не було що 0x = 3
     pass
 
 
 def add_vector(matrix, vector):
     if number_of_rows(matrix) == number_of_rows(vector):
         for i in range(len(vector)):
-            matrix[i].append(vect[i])
+            matrix[i].append(vector[i])
         return matrix
     else:
         return "Wrong vector"
